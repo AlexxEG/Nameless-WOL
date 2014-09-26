3f14983fae66cbec57e7ccbfddd260ac9d1002dc
Alexander Ellingsen <alexellingsen@gmail.com>
2014-09-26 16:49:38 +0200

    Add underscore to TAG for easier filtering

--- src/com/gmail/alexellingsen/namelesswol/utils/Common.java
+++ src/com/gmail/alexellingsen/namelesswol/utils/Common.java
@@ -3,7 +3,7 @@ package com.gmail.alexellingsen.namelesswol.utils;
 public class Common {
 
     public static String INTENT_EXTRA_DEVICE_ID = "wake_device_id";
-    public static String TAG = "NamelessWOL";
+    public static String TAG = "Nameless_WOL";
 
     public static String TASKER_EXTRA_BUNDLE = "com.twofortyfouram.locale.intent.extra.BUNDLE";
     public static String TASKER_EXTRA_STRING_BLURB = "com.twofortyfouram.locale.intent.extra.BLURB";
